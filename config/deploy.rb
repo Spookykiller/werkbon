c4a4fdb9c56d304265fc66a70efbf0cb8a73bbd9
Gijs Verdonschot <flexkoenders@koendersadvies.nl>
2016-09-20 16:10:31 +0000

    Change deploy configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :rvm_ruby_version, '2.3.0'
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-set :rvm_type, :user                     # Defaults to: :auto
+set :rvm_type, :system                     # Defaults to: :auto
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
