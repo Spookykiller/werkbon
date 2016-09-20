be83b7ddc1980305a8ca4654a52cf0d7ec3bff22
Gijs Verdonschot <flexkoenders@koendersadvies.nl>
2016-09-20 06:52:55 +0000

    Add rvm_type :user in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,8 @@ set :rvm_ruby_version, '2.3.0'
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
+set :rvm_type, :user                     # Defaults to: :auto
+
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
