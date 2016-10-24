e5b742473819460526668886fea7aefbbc1abefa
Gijs Verdonschot <flexkoenders@koendersadvies.nl>
2016-10-24 13:01:36 +0000

    Add lockable to authorization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922182408) do
+ActiveRecord::Schema.define(version: 20161024130014) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "omschrijving"
@@ -126,6 +126,8 @@ ActiveRecord::Schema.define(version: 20160922182408) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.datetime "locked_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
